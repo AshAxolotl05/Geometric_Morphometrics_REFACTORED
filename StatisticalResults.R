4bbc70d9eaf326acaf52d8525be498c4d2e2413a
AshAxolotl <scullin19@gmail.com>
2025-12-19 11:49:45 -0500

    statistical results comments

--- StatisticalResults.R
+++ StatisticalResults.R
@@ -3,8 +3,13 @@ library('geomorph')
 library('Morpho')
 library('abind')
 library('magick')
+library('roxygen2')
 
-# save last rgl window as a gif
+#' Saves last rgl window as a GIF int he current directory.
+#'
+#' @param name A base name to use for the gif, not including any surface or species information.
+#' @param species The species that was plotted.
+#' @param surface The surface (white matter or gray matter) that was plotted
 makeGif = function(name, species, surface) {
   r3dDefaults$windowRect = c(100, 100, 1000, 1000)
 
@@ -15,7 +20,10 @@ makeGif = function(name, species, surface) {
     type = "gif", clean = T, fps=10, webshot=F, dir='.')
 }
 
-# add species and age as factors to gpagen object
+#' Add species and age group (based on developmental milestones for humans and macaques up to Late Childhood) as factors to a gpagen object.
+#'
+#' @param gpa The gpagen object to modify.
+#' @param specimens A list of specimen ids in the format [subject]_[time]_[species] (1_12months_macaque). These must be in the same order as specimens in the gpa.
 addFactors = function (gpa, specimens) {
   # get subject number
   subject = c()
@@ -80,7 +88,10 @@ addFactors = function (gpa, specimens) {
   return(gpa)
 }
 
-
+#' Performs Pearson's correlation coefficent tests for the first four principal components and centroid size.
+#'
+#' @param PCA An object of class gm.prcomp to obtain relevant pc scores from.
+#' @param gpa An object of class gpagen to obtain centroid sizes from.
 pcToSize = function(PCA, gpa) {
 
   #look for correlation to centroid size
@@ -97,7 +108,11 @@ pcToSize = function(PCA, gpa) {
   print(cor.test(pc4,gpa$Csize))
 }
 
-
+#' Performs analysis of variance tests to investgate the effects of centroid size, age, and potentially species on overall shape.
+#'
+#' @param gdf A geomorph data.frame containing GPA results as well as age (and species if species=TRUE) as a factor.
+#' @param gpa A gpagen object containing results of GPA with age (and species if species=TRUE) as a factor.
+#' @species A boolean describing whether species should be included as a covariate(only necesssary for cross-species analysis).
 multivariateAnalysis = function(gdf, gpa, species=FALSE) {
   #effects of age group, species and centroid size on shape
 
@@ -120,6 +135,10 @@ multivariateAnalysis = function(gdf, gpa, species=FALSE) {
   return(modelInteraction)
 }
 
+#' Finds the separate consensus configurations for macaques and humans after a combined GPA.
+#' The result is a 3D matrix with the macaque configuration as the first 'slice' (accessible by [,,1] and the human configuration second (accessible by [,,2])
+#'
+#' @param gpa A gpagen object containing the results of cross-species GPA
 getBySpeciesConsensus = function(gpa) {
   humanMatrix =  gpa$coords[,,grepl('human', dimnames(gpa$coords)[[3]])]
   macaqueMatrix =  gpa$coords[,,grepl('macaque', dimnames(gpa$coords)[[3]])]
@@ -132,25 +151,34 @@ getBySpeciesConsensus = function(gpa) {
   return(abind(macaqueConsensus, humanConsensus, along=3))
 }
 
+#' Creates a GIF visually transforming a configuration of landmarks into another and back.
+#'
+#' @param config1 The first configuration as a 2D matrix of coordinates, formatted so x, y and optionally z are columns.
+#' @param config2 The first configuration as a 2D matrix of coordinates, formatted so x, y and optionally z are columns..
+#' @param path The path to save the resulting GIF to. Defaults to the current directory.
 transformConfigurations = function(config1, config2, path='.') {
 
-#transform macaque to human
-warpmovie3d(config1, config2, 15, col = 'green', palindrome = TRUE,
-            folder = '.', movie = paste('combined', surface, 'consensusPoints', sep='_'))
+  #transform macaque to human
+  warpmovie3d(config1, config2, 15, col = 'green', palindrome = TRUE,
+              folder = '.', movie = paste('combined', surface, 'consensusPoints', sep='_'))
 
-#merge pngs to gif
-list.files(path=path,
-           pattern = '*.png', full.names = TRUE) %>%
-        image_read() %>% # reads each path file
-        image_join() %>% # joins image
-        image_animate(fps=4) %>% # animates, can opt for number of loops
-        image_write(format='gif', path=paste(paste('combined', surface, 'consensusPoints', sep='_'), '.gif', sep='')) # write to current dir
+  #merge pngs to gif
+  list.files(path=path,
+             pattern = '*.png', full.names = TRUE) %>%
+          image_read() %>% # reads each path file
+          image_join() %>% # joins image
+          image_animate(fps=4) %>% # animates, can opt for number of loops
+          image_write(format='gif', path=paste(paste('combined', surface, 'consensusPoints', sep='_'), '.gif', sep='')) # write to current dir
 
-#teardown pngs
-list.files(path=path,
-           pattern = '*.png', full.names = TRUE) %>% unlink()
+  #teardown pngs
+  list.files(path=path,
+             pattern = '*.png', full.names = TRUE) %>% unlink()
 }
 
+#' Plots deviation associated with the extreme ends of a single principal compoenent. The minimum value is plotted in blue, the maximum in green.
+#'
+#' @param pca A gm.prcomp object containing PCA information.
+#' @param pc The number (1, 2, 3 etc) of the principal component to look at. Default is 1 for pc1.
 deviationAlongPC = function(pca, pc=1) {
   comp = paste0('shapes.comp', pc)
 
